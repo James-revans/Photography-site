ad27600d1d4d5d31fb115d58a766dd2b8eb7404b
James Evans <james@Jamess-MacBook-Pro.local>
2019-11-14 15:39:55 -0800

    Fixed sizing of recent photos, added loader for recent photos

--- src/components/home/Recent.svelte
+++ src/components/home/Recent.svelte
@@ -63,7 +63,7 @@ onMount(async () => {
             padding: 15px 0px;
             img {
                 width: 200px;
-                height: 200px;
+                height: 300px;
                 object-fit: cover;
             }
         }
@@ -71,6 +71,45 @@ onMount(async () => {
     @include break-up('sm') {
         width: 80%;
     }
+}
+
+
+
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 64px;
+  height: 64px;
+  margin: 0 auto;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 51px;
+  height: 51px;
+  margin: 6px;
+  border: 6px solid #445B47;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #445B47 transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 
 </style>
@@ -83,6 +122,9 @@ onMount(async () => {
         {#each recentArray as item}
             <div class="recent__photos__photo"><img src="{item}" alt="SG recent photos"/></div>
         {/each}
+    {:else} 
+        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
     {/if}
+
     </div>
 </div>
