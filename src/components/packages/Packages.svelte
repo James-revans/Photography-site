052ab4836d60fe48bf284c4d560b1c45e17aa1ca
Devin Heigert <heigertd@gmail.com>
2020-10-07 17:26:36 -0700

    basic layout finished

--- src/components/packages/Packages.svelte
+++ src/components/packages/Packages.svelte
@@ -5,18 +5,46 @@
     import PackageDescription from "./Packages-description.svelte"
     const url = './images/Contact-Banner-min.jpg';
 </script>
+<style>
+    .descriptions{
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h1{
+        font-family: 'Permanent Marker', cursive;
+        font-size: 3vw;
+    }
+
+    p{
+        font-size: 1.25vw;
+    }
+
+    .package-text{
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+</style>
 
 
 <PageBanner img={url}>
     <h1>Packages</h1>
 </PageBanner>
-<h3>How it Works</h3>
-<p>So I’ll post the photos in a Pixieset folder that the client will have access to holding about the number of images the package comes with.</p>
+<div class='package-text'>
+    <h1>How it Works</h1>
+    <p>So I’ll post the photos in a Pixieset folder that the client will have access to holding about the number of images the package comes with.</p>
+</div>
 
-{#each packages as packages}
-    <PackageDescription {...packages} />
-{/each}
-<h3>Disclaimer</h3>
-<p>I’ll have a little disclaimer at the bottom of the page saying I can compromise on different options or whatever through communication.</p>
-
+<div class='descriptions'>
+    {#each packages as packages}
+        <PackageDescription {...packages} />
+    {/each}
+</div>
+<div class='package-text'>
+    <h1>Disclaimer</h1>
+    <p>I’ll have a little disclaimer at the bottom of the page saying I can compromise on different options or whatever through communication.</p>
+</div>
 <Footer/>
--- src/components/packages/package-data.js
+++ src/components/packages/package-data.js
@@ -1,27 +1,30 @@
 export const packages = [
     {
-        name: 'A',
+        name: 'Package A',
         length: '30min',
         photos: '20',
         outfits: '1-2',
         location: '1',
-        price: '75'
+        price: '75',
+        image: 'https://res.cloudinary.com/savanna-photos/image/upload/v1585161531/media/IMG_0650.JPG.jpg'
     },
     {
-        name: 'B',
+        name: 'Package B',
         length: '60min',
         photos: '40',
         outfits: '1-3',
         location: '1-2',
-        price: '175'
+        price: '175',
+        image: 'https://res.cloudinary.com/savanna-photos/image/upload/v1585161531/media/IMG_0650.JPG.jpg'
     },
     {
-        name: 'C',
+        name: 'PAckage C',
         length: '90min',
         photos: '60',
         outfits: '1-4',
         location: '1-3',
-        price: '250'
+        price: '250',
+        image: 'https://res.cloudinary.com/savanna-photos/image/upload/v1585161531/media/IMG_0650.JPG.jpg'
     },
 
 ];
